6d3243b61dbef86efdb619703c6711e52f316d73
Terretino <terretino@10-61-51-20.0200.bin.id.iosda.org>
2018-06-04 11:37:51 +0700

    Bagusin View Controller

--- GiveBloodApp/GiveBloodApp/Page ViewController.swift
+++ GiveBloodApp/GiveBloodApp/Page ViewController.swift
@@ -8,34 +8,86 @@
 
 import UIKit
 
-class Page_ViewController: UIPageViewController, UIPageViewControllerDelegate {
+class Page_ViewController: UIPageViewController, UIPageViewControllerDelegate, UIPageViewControllerDataSource {
     
     lazy var orderedViewController: [UIViewController] = {
         return [self.newVC(viewController: "pg1"),
         self.newVC(viewController: "pg2"),
-        self.newVC(viewController: "pg3")]()
-    }
+        self.newVC(viewController: "pg3")]
+    }()
+    
+    var pageControl = UIPageControl()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.delegate = self
+        self.dataSource = self
+        if let firstView = orderedViewController.first {
+            setViewControllers([firstView], direction: .forward, animated: true, completion: nil)
+        }
+        configurePageControl()
 
         // Do any additional setup after loading the view.
     }
     
-    func newVC(viewController: String) -> UIViewController{
-        return UIStoryboard(name: "main", bundle: nil).instantiateInitialViewController(withIddentifier: ViewController)
+    func configurePageControl(){
+        pageControl = UIPageControl(frame: CGRect(x: 0, y: UIScreen.main.bounds.maxY - 50, width: UIScreen.main.bounds.width, height: 50))
+        pageControl.numberOfPages = orderedViewController.count
+        pageControl.currentPage = 0
+        pageControl.tintColor = UIColor.black
+        pageControl.pageIndicatorTintColor = UIColor.white
+        pageControl.currentPageIndicatorTintColor = UIColor.black
+        self.view.addSubview(pageControl)
     }
     
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController?{
-        guard let viewControllerIndex = orderedViewController.index(of: viewController) {
-            else return nil
+    func newVC(viewController: String) -> UIViewController{
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: viewController)
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewController.index(of: viewController) else {
+            return nil
         }
         
         let previousIndex = viewControllerIndex - 1
-        guard let previousIndex >= 0 {
-            else return orderedViewController.last
+        
+        guard previousIndex >= 0 else {
+            //return orderedViewController.last
+            return nil
         }
+        
+        guard orderedViewController.count > previousIndex else {
+            return nil
+        }
+        
+        return orderedViewController[previousIndex]
     }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewController.index(of: viewController) else {
+            return nil
+        }
+        let nextIndex = viewControllerIndex + 1
+        guard orderedViewController.count != nextIndex else {
+            //return orderedViewController.first
+            return nil
+        }
+        
+        guard orderedViewController.count > nextIndex else {
+            return nil
+        }
+        
+        return orderedViewController[nextIndex]
+        
+        
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        let pageContentViewController = pageViewController.viewControllers![0]
+        self.pageControl.currentPage = orderedViewController.index(of: pageContentViewController)!
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
---
+++ GiveBloodApp/GiveBloodApp/ViewControllerPG2.swift
@@ -0,0 +1,37 @@
+//
+//  ViewControllerPG2.swift
+//  GiveBloodApp
+//
+//  Created by Terretino on 04/06/18.
+//  Copyright © 2018 Kristopher Chayadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerPG2: UIPageViewController {
+
+    //@IBOutlet weak var labelAtas: UILabel!
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        //labelAtas.text = String?("Give Blood Mengingatkan anda")
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
