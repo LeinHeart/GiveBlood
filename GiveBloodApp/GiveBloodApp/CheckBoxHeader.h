187084a4b5d18ef14a68be9e4f294788422a4619
Terretino <terretino@10-61-51-20.0200.bin.id.iosda.org>
2018-06-05 08:54:14 +0700

    tambahin picker view, kurang ambil data di data

---
+++ GiveBloodApp/GiveBloodApp/CheckBoxHeader.h
@@ -0,0 +1,13 @@
+//
+//  CheckBoxHeader.h
+//  GiveBloodApp
+//
+//  Created by Terretino on 05/06/18.
+//  Copyright © 2018 Kristopher Chayadi. All rights reserved.
+//
+
+#ifndef CheckBoxHeader_h
+#define CheckBoxHeader_h
+
+
+#endif /* CheckBoxHeader_h */
---
+++ GiveBloodApp/GiveBloodApp/ViewControllerIB1.swift
@@ -0,0 +1,69 @@
+//
+//  ViewControllerIB1.swift
+//  GiveBloodApp
+//
+//  Created by Terretino on 04/06/18.
+//  Copyright © 2018 Kristopher Chayadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerIB1: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+
+    let golDar = ["AB+", "A+", "B+", "O+", "O-", "B-", "A-", "AB-", "Tidak Tahu"]
+    
+    @IBOutlet weak var buttonTitle: UIButton!
+    @IBOutlet weak var pickerView: UIPickerView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        pickerView.isHidden = true
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    
+    // returns the # of rows in each component..
+    @IBAction func pilihButton(_ sender: Any) {
+        if pickerView.isHidden {
+            pickerView.isHidden = false
+        }
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return golDar.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return golDar[row]
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        buttonTitle.setTitle(golDar[row], for: .normal)
+        pickerView.isHidden = true
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
